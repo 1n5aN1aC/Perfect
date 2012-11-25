4d7f6b52d1f9af22d1e217e5e704e46c22f83d3f
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-24 16:15:17 -0800

    name changes

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import socket
 
 HOST = 'daring.cwi.nl'    # The remote host
 PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+sock.sendall('Hello, world')
+data = sock.recv(1024)
+sock.close()
 print 'Received', repr(data)
