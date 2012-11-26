b665108f582cb4a00d3263e5479c904a315e7eb5
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-26 14:38:40 -0800

    Server keeps listening.
    Client sands JSON and receives JSON now.
    JSON parsing not completed yet, however.

--- src/client.py
+++ src/client.py
@@ -1,11 +1,23 @@
 # Echo client program
 import socket
+import json
+from StringIO import StringIO
+def as_complex(dct):
+    return complex(dct['packet'], dct['data'])
+    return dct
+dataToSend = StringIO()
+
+json.dump([{"id":1, "stuff":"streaming API"}], dataToSend)
 
 HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+s.sendall( dataToSend.getvalue() )
 data = s.recv(1024)
 s.close()
+
 print 'Received', repr(data)
+jdata = json.loads(data)
+
+print 'Parsed', repr(jdata)
